1dffe1245b5b94a699c04290cebb38600384dc51
momenkittaneh <momen.kittaneh93@outlook.com>
2021-06-13 12:45:24 +0300

    exception

---
+++ java basics/listsofexceptions/ListsofExceptions.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listsofexceptions;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author black
+ */
+public class ListsofExceptions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+myList.add("13");
+myList.add("hello world");
+myList.add(48);
+myList.add("Goodbye World");
+for(int i = 0; i < myList.size(); i++) {
+    try{
+    Integer castedValue = (Integer) myList.get(i);}
+    catch(Exception e){
+        System.out.println(e);
+    }
+}
+
+    }
+    
+}
