8c4be4c3cc5b6348419ffd808e628d512ff8fe21
momenkittaneh <momen.kittaneh93@outlook.com>
2021-06-13 09:35:13 +0300

    java
    first assigment

---
+++ java basics/FirstJavaProgram/src/firstjavaprogram/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+package firstjavaprogram;
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+       System.out.println("My name is Momen Kittaneh");
+       System.out.println("I am 27 years old");
+       System.out.println("My hometown is Tulkarem, WestBank");
+    }
+    
+}
