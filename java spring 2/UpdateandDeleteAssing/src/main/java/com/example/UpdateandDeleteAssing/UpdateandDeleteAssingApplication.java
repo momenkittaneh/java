8bf05cf85e45a324b6cf74989d66fe2aed399f08
momenkittaneh <momen.kittaneh93@outlook.com>
2021-06-26 23:22:40 +0300

    update

---
+++ java spring 2/UpdateandDeleteAssing/src/main/java/com/example/UpdateandDeleteAssing/UpdateandDeleteAssingApplication.java
@@ -0,0 +1,13 @@
+package com.example.UpdateandDeleteAssing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateandDeleteAssingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateandDeleteAssingApplication.class, args);
+	}
+
+}
